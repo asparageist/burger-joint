50538ca63ece6ca002ef7e80eb5fc132dcc861b2
Joseph Murray <70991796+asparageist@users.noreply.github.com>
2024-01-13 15:09:55 -0800

    button alternates between Control and NewInvForm.

---
+++ src/components/NewInvForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewInvForm(props) {
+  return (
+    <React.Fragment>
+      <h3>there be forms here</h3>
+    </React.Fragment>
+  );
+}
+
+export default NewInvForm;
--- src/components/invControl.js
+++ src/components/invControl.js
@@ -1,19 +1,39 @@
 import React from "react";
 import Inventory from "./Inventory";
+import InvList from "./InvList";
+import NewInvForm from "./NewInvForm";
 
 class InvControl extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
   }
 
   render(){
+    let currentlyVisibleState = null;
+    let buttonText = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewInvForm />
+      buttonText="BACK TO THE BURGERS";
+    } else {
+      currentlyVisibleState = <InvList />
+      buttonText="NEW BURGER";
+    }
     return (
       <React.Fragment>
         <div className="content">
-        <p>boop the snoots</p>
-        <Inventory />
+          <p>boop the snoots</p>
+          {currentlyVisibleState}
+          <button onClick={this.handleClick}>{buttonText}</button>
         </div>
       </React.Fragment>
     );
